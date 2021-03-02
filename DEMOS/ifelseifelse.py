2b20b71ae418dd0012b84ca1c3b6879f32cf6d55
repl.it user <replituser@example.com>
2021-03-02 11:18:35 +0000

    datatypes

--- DEMOS/ifelseifelse.py
+++ DEMOS/ifelseifelse.py
@@ -1,17 +1,17 @@
-print("What is your name")
+print("What is your name?")
 n = input()
-#print("Do you have a dog (types True of False)")
-#dog = input()
+# print("Do you have a dog? (type True or False)")
+# dog = input()
 #bool is boolean datatype - only stores True/False
 
 
-if len(n) > 9:
-  print ("you have a very loooong name!")
-  print("your name has {} letters".format(len(n)))
-elif len(n) >6:
-  print("your name is a bt long. Consider a nickname")
-elif len(n) <3:
-    print("your name is veeery short")
+if len(n) >= 9: # allow length of exactly 9 and greater
+  print("You have a very loooong name!")
+  print ("Your name contains {} letters".format(len(n)))
+elif len(n) > 6:
+  print("Your name is a bit long. Consider a nickname")
+elif len(n) > 3:
+  print("your name is veeeery short")
 else:
-  print("your name is quite okay")
+  print("Your name is quite okay")
 print("This is the END of the program!")
--- gitcycle.py
+++
@@ -1,4 +0,0 @@
-print ("Anything i want")
-print ("git add.")
-print ("second step : git commit -m \"you message\"")
-print("third step: git push")
