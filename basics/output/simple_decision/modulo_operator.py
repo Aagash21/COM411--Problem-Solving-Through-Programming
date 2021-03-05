b5df31a5e4aa234d5868531da07b01be1b775995
repl.it user <replituser@example.com>
2021-03-05 10:45:55 +0000

    ok

---
+++ basics/output/simple_decision/modulo_operator.py
@@ -0,0 +1,6 @@
+print("please enter a whole number")
+n = int(input())
+if (n % 2 == 0):
+    print("\n{} is an even number".format(n))
+else:
+    print("\n{} is an odd number".format(n))
--- basics/output/simple_decision/modulo_operator.py.py
+++
@@ -1,9 +0,0 @@
-print("please enter a whole number")
-n=int(input())
-if (n%2==0):
-  print("{} is an even number".format(n))
-else:
-    print("{} is an odd number".format(n))
-
- 
-  
